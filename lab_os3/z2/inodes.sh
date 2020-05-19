cb45bfac18734aaa0e407502ae5b9905d1d63f38
realkarmakun <20980281+realkarmakun@users.noreply.github.com>
2020-05-20 01:12:07 +0500

    Фикс 3 лабы, 2 задания

--- lab_os3/z2/inodes.sh
+++ lab_os3/z2/inodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 > otchet.txt
-ls -li $1 | sort -n >> otchet.txt
+ls -lip $1| grep -v / | sort -n >> otchet.txt
 chmod 777 otchet.txt
 ln otchet.txt ~
 ln -s ~/OtherProjects/lab_os/z2/otchet.txt ~/Desktop
